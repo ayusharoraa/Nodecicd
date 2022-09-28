3a58f714ab10db367703b1b3dac4d74f58c606ad
Ubuntu <ubuntu@ip-172-31-95-170.ec2.internal>
2022-09-28 11:39:16 +0000

    prac-to-the-tice

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -3,6 +3,6 @@ cd /home/ubuntu
 mkdir server
 cd server
 npm install
-#npm install pm2 -gg
+#3#npm install pm2 -gg
 sudo npm install pm2@latest -g
 sudo sh -c 'echo "$(hostname -I) localhost" > /etc/hosts'
