a9e0dbce5187e288bf72d6a2f66e353d2e44f3bf
Ubuntu <ubuntu@ip-172-31-95-170.ec2.internal>
2022-09-28 11:49:36 +0000

    idjasj

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! this is ayushs wbesite on hi\n');
+  res.end('Hello World! this is ayushs webbie\n');
 });
 
 server.listen(port, hostname, () => {
